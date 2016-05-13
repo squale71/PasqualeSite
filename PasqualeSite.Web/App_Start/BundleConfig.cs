dcc0098e041b05b2ac9a08f0c4c31ce826b278c7
squale71 <plcaiazzo@gmail.com>
2016-05-13 16:34:29 -0400

    Added some optimizations

---
+++ PasqualeSite.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace PasqualeSite.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            BundleTable.EnableOptimizations = true;
+            bundles.Add(new ScriptBundle("~/bundles/sitejs").Include(
+                        "~/js/jquery-1.9.1.min.js",
+                        "~/js/plugins/jqueryui/jquery-ui.min.js",
+                        "~/js/bootstrap.min.js",
+                        "~/js/plugins/owl/owl.carousel.min.js"
+            ));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/workareajs").Include(
+                        "~/js/knockout-3.4.0.js",
+                        "~/js/knockout-mapping.js",
+                        "~/js/knockout.mapping.merge.js",
+                        "~/js/plugins/kogrid/koGrid-2.1.1.js",
+                        "~/js/knockouthandlers.js",
+                        "~/js/plugins/datejs/date.js",
+                        "~/js/plugins/notify/notify.min.js",
+                        "~/js/plugins/bootstrap-multiselect/bootstrap-multiselect.js"
+            ));
+
+            bundles.Add(new StyleBundle("~/Content/sitecss").Include(
+                        "~/css/bootstrap.min.css",
+                        "~/js/plugins/owl/assets/owl.carousel.css",
+                        "~/js/plugins/owl/assets/owl.theme.css",
+                        "~/css/app.css"
+            ));
+
+            bundles.Add(new StyleBundle("~/Content/workareacss").Include(
+                        "~/js/plugins/kogrid/KoGrid.css",
+                        "~/js/plugins/bootstrap-multiselect/bootstrap-multiselect.css"
+            ));
+        }
+    }
+}
--- PasqualeSite.Web/Global.asax.cs
+++ PasqualeSite.Web/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using PasqualeSite.Data.Database;
+using PasqualeSite.Web.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace PasqualeSite.Web
@@ -15,6 +17,7 @@ namespace PasqualeSite.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             // Fill database using EF Code First.
             //TODO: Comment in Production
