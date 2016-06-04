138180ee35309e1b231f20ccdd8c4c0fc36127b7
squale71 <plcaiazzo@gmail.com>
2016-06-03 23:45:25 -0400

    fixed issue with instagram. added privacy page.

---
+++ PasqualeSite.Web/Controllers/InstagramController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Caching;
+using System.Text;
+using System.Web.Http;
+
+namespace PasqualeSite.Web.Controllers
+{
+    public class InstagramController : ApiController
+    {
+        public ObjectCache cache = MemoryCache.Default;
+        public CacheItemPolicy policy = new CacheItemPolicy();
+
+        // GET: api/instagram
+        public HttpResponseMessage Get(string id)
+        {
+            var json = "";
+            using (var wc = new WebClient())
+            {
+                json = wc.DownloadString("https://www.instagram.com/" + id + "/media");
+            }
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(
+                        json,
+                        Encoding.UTF8,
+                       "application/json"
+                    )
+            };
+        }
+    }
+}
